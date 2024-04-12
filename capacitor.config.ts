9cdf7decb3263b7fee28e7f3c1edb11caff119ed
DHARMESH SHARMA <sharmadharmesh1998@gmail.com>
2024-04-12 14:19:13 +0530

    google auth added for web

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.capacitor_test.app',
   appName: 'hello_capacitor',
   webDir: 'build',
   bundledWebRuntime: false,
@@ -9,7 +9,17 @@ const config: CapacitorConfig = {
     // url: 'http://192.168.137.1:3000',
     url: 'https://capacitor-test-ten.vercel.app/',
     cleartext: true
+  },
+  "plugins": {
+    "GoogleAuth": {
+      "scopes": ["profile", "email"],
+      "serverClientId": "188386290850-9fp1l8ckjp146sh83djf2tdfk7fq478i.apps.googleusercontent.com",
+      "forceCodeForRefreshToken": true
+    }
   }
 };
 
 export default config;
+
+// id: 188386290850-9fp1l8ckjp146sh83djf2tdfk7fq478i.apps.googleusercontent.com
+// secret: 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
--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { GoogleAuth } from "@codetrix-studio/capacitor-google-auth";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World Part three!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Capacitor
-        </a>
-      </header>
-    </div>
-  );
+	GoogleAuth.initialize({
+		clientId:
+			"188386290850-9fp1l8ckjp146sh83djf2tdfk7fq478i.apps.googleusercontent.com",
+		scopes: ["profile", "email"],
+		grantOfflineAccess: true,
+	});
+
+	// useEffect(()=>{
+	//   GoogleAuth.init();
+	// },[])
+
+	const [user, setUser] = useState(null);
+
+	const signIn = async () => {
+		try {
+			const userData = await GoogleAuth.signIn();
+			console.log(userData);
+			setUser(userData);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const refresh = async () => {
+		try {
+			const authCode = await GoogleAuth.refresh();
+			console.log("refresh: ", authCode);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const signOut = async () => {
+		try {
+			await GoogleAuth.signOut();
+			console.log("user Signed out!");
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Welcome!</p>
+				<button onClick={signIn}>Login with google</button>
+				{user !== null ? <p>{user.email}</p> : null}
+			</header>
+		</div>
+	);
 }
 
 export default App;
